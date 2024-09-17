f4a002542240d8ae34647ffa982141fce58e6477
Honz Williams <hnzwllms@gmail.com>
2024-09-17 10:29:01 -0400

    keyboard dismiss for physical device

--- src/components/NoteModal.jsx
+++ src/components/NoteModal.jsx
@@ -6,6 +6,10 @@ import {
   Text,
   TextInput,
   StyleSheet,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
 } from 'react-native';
 
 const NoteModal = ({
@@ -26,69 +30,75 @@ const NoteModal = ({
       transparent={true}
       visible={modalVisible}
       onRequestClose={() => setModalVisible(false)}>
-      <View style={styles.modalView}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="title."
-            style={styles.input}
-            value={title}
-            onChangeText={text => {
-              setTitle(text);
-              setErrorMessage('');
-            }}
-          />
-          {errorMessage && (
-            <Text style={styles.errorMessage}>{errorMessage}</Text>
-          )}
-          <TextInput
-            placeholder={errorMessage ? '' : 'body.'}
-            style={styles.bodyInput}
-            value={body}
-            multiline
-            editable
-            numberOfLines={6}
-            onChangeText={text => {
-              setBody(text);
-              setErrorMessage('');
-            }}
-          />
-        </View>
-        <View style={styles.modalButtonContainer}>
-          <Pressable
-            onPress={handleCancel}
-            style={({pressed}) => [
-              {
-                borderRightWidth: 2,
-                borderColor: '#acaec6',
-              },
-            ]}>
-            {({pressed}) => (
-              <Text
-                style={[
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.modalView}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={{flex: 1}}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="title."
+                style={styles.input}
+                value={title}
+                onChangeText={text => {
+                  setTitle(text);
+                  setErrorMessage('');
+                }}
+              />
+              {errorMessage && (
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
+              )}
+              <TextInput
+                placeholder={errorMessage ? '' : 'body.'}
+                style={styles.bodyInput}
+                value={body}
+                multiline
+                editable
+                numberOfLines={6}
+                onChangeText={text => {
+                  setBody(text);
+                  setErrorMessage('');
+                }}
+              />
+            </View>
+            <View style={styles.modalButtonContainer}>
+              <Pressable
+                onPress={handleCancel}
+                style={({pressed}) => [
                   {
-                    color: pressed ? '#acaec6' : '#3c3e5f',
+                    borderRightWidth: 2,
+                    borderColor: '#acaec6',
                   },
-                  styles.buttonText,
                 ]}>
-                Cancel
-              </Text>
-            )}
-          </Pressable>
-          <Pressable onPress={handleSaveNote}>
-            {({pressed}) => (
-              <Text
-                style={[
-                  {
-                    color: pressed ? '#acaec6' : '#3c3e5f',
-                  },
-                  styles.buttonText,
-                ]}>
-                Save
-              </Text>
-            )}
-          </Pressable>
+                {({pressed}) => (
+                  <Text
+                    style={[
+                      {
+                        color: pressed ? '#acaec6' : '#3c3e5f',
+                      },
+                      styles.buttonText,
+                    ]}>
+                    Cancel
+                  </Text>
+                )}
+              </Pressable>
+              <Pressable onPress={handleSaveNote}>
+                {({pressed}) => (
+                  <Text
+                    style={[
+                      {
+                        color: pressed ? '#acaec6' : '#3c3e5f',
+                      },
+                      styles.buttonText,
+                    ]}>
+                    Save
+                  </Text>
+                )}
+              </Pressable>
+            </View>
+          </KeyboardAvoidingView>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
